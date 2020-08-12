96f4b3a336c45a945ef3fb9f41ed21e6f32ece19
Runebreaker <gino.schulze-skyfire@web.de>
2020-08-13 01:54:14 +0200

    Added basic functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -3,70 +3,119 @@
 #include <WinUser.h>
 #include <stdio.h>
 #include <string>
+#include <algorithm>
 
-class Twanswator {
-    public:
-    int input = 0;
-    int wastinput = 1;
-    bool wunning = 1;
+bool wunning = 1;
 
-    HANDLE h;
+HANDLE h = GetClipboardData(CF_TEXT);
+HANDLE last = GetClipboardData(CF_TEXT);
 
-    Twanswator() {
+/*
+void BailOut(char *msg)
+{
+    fprintf(stderr, "Exiting: %s\n", msg);
+    exit(1);
+}
 
-    }
+int getText()
+{
+    if (OpenClipboard(NULL)) {
 
-    void BailOut(char *msg)
-    {
-        fprintf(stderr, "Exiting: %s\n", msg);
-        exit(1);
-    }
-
-    int getText() {
-        if (!OpenClipboard(NULL))
-        BailOut("Can't open clipboard");
-   
+        last = h;
         h = GetClipboardData(CF_TEXT);
- 
         CloseClipboard();
-        return 0;
+
     }
 
-    int setText(std::string* in) {
-        if (!OpenClipboard(NULL))
-        BailOut("Can't open clipboard");
-   
+    return 0;
+}
+
+int setText(std::string *in)
+{
+    if (OpenClipboard(NULL)) {
+
+        h = in;
         SetClipboardData(1, h);
- 
         CloseClipboard();
-        return 0;
+
     }
 
-    std::string* translate(std::string tInput) {
-        getText();
+    return 0;
+}
+*/
 
-	    for (std::string::size_type i = 0; i < tInput.size(); i++) {
-            if(tInput[i] == 'L' || tInput[i] == 'R') {
-                tInput[i] = 'W';
+std::string translate(std::string tInput)
+{
+    int* toBeRemoved = new int[tInput.size()];
+    int position = 0;
+    int uwuCounter = 0;
+    for (int i = 0; i < tInput.size(); i++)
+    {
+        if (tInput[i] == 'L' || tInput[i] == 'R') //Basics
+        {
+            tInput[i] = 'W';
+            uwuCounter++;
+        }
+        else if (tInput[i] == 'l' || tInput[i] == 'r') //Basics
+        {
+            tInput[i] = 'w';
+            uwuCounter++;
+        }
+        else if ((tInput[i] == 'T' || tInput[i] == 't')) //Replace the -> te, this -> tis
+        {
+            if((i + 2) <= tInput.size() && (tInput[i + 1] == 'h' || tInput[i + 1] == 'H') && (tInput[i + 2] == 'e' || tInput[i + 2] == 'E')) {
+                tInput[i + 1] = 'e';
+                tInput[i + 2] = ' ';
+                toBeRemoved[position] = (i + 2);
+                position++;
             }
-            else if(tInput[i] == 'l' || tInput[i] == 'r') {
-                tInput[i] = 'w';
-            }
-	    }
-
-        return &tInput;
-    }
-
-    void run() {
-        while(wunning) {
-            std::string out;
-            if(input == VK_MENU && input != wastinput) {
-                setText(translate(*(std::string*)h));
-                wastinput = input;
-            }
-            else if(input == VK_RCONTROL) {
-                wunning = 0;
+            if((i + 3) <= tInput.size() && (tInput[i + 1] == 'h' || tInput[i + 1] == 'H') && (tInput[i + 2] == 'i' || tInput[i + 2] == 'I') && (tInput[i + 3] == 's' || tInput[i + 3] == 'S')) {
+                tInput[i] = 'D';
+                tInput[i + 1] = 'i';
+                tInput[i + 2] = 's';
+                tInput[i + 3] = ' ';
+                toBeRemoved[position] = (i + 3);
+                position++;
             }
         }
     }
-} twanswator;
+
+    for(int i = 0; i < position; i++) {
+        tInput.erase(toBeRemoved[i], 1);
+    }
+
+    delete toBeRemoved;
+
+    return tInput;
+}
+
+/*void run()
+{
+    while (wunning)
+    {
+        getText();
+        if(h != last)
+        {
+            setText(translate(*(std::string *)h));
+        }
+    }
+}*/ //Leads to segfault
+
+std::string textBasedTranslation(std::string in) {
+    std::string out = "";
+    out = translate(in);
+    return out;
+}
+
+int main()
+{
+    //run();
+    std::string input;
+    std::cout << "What do you want to twanswate? UwU" << std::endl << std::endl << "\t";
+    std::getline(std::cin, input);
+    std::cout << std::endl << "Twanswating... UwU" << std::endl << std::endl
+    << "\t" << textBasedTranslation(input) << std::endl << std::endl
+    << "Thank you fow using my twanswator OwO" << std::endl << std::endl;
+    system("pause");
+    return 0;
+}
