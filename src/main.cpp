44bf25bdb28d1ee1e1953f8bafd578b824340848
Runebreaker <gino.schulze-skyfire@web.de>
2020-08-13 02:58:59 +0200

    Tried fixing Clipboard issue, somehow access is denied (Error code 5)

--- src/main.cpp
+++ src/main.cpp
@@ -7,46 +7,54 @@
 
 bool wunning = 1;
 
-HANDLE h = GetClipboardData(CF_TEXT);
-HANDLE last = GetClipboardData(CF_TEXT);
+std::string h = " ";
+std::string last = " ";
 
-/*
-void BailOut(char *msg)
+/*std::string getText()
 {
-    fprintf(stderr, "Exiting: %s\n", msg);
-    exit(1);
+    // Try opening the clipboard
+    if (!OpenClipboard(nullptr))
+        std::cout << GetLastError() << std::endl; // error
+
+    // Get handle of clipboard object for ANSI text
+    HANDLE hData = GetClipboardData(CF_TEXT);
+    if (hData == nullptr)
+        std::cout << GetLastError() << std::endl; // error
+
+    // Lock the handle to get the actual text pointer
+    char *pszText = static_cast<char *>(GlobalLock(hData));
+    if (pszText == nullptr)
+        std::cout << GetLastError() << std::endl; // error
+
+    // Save text in a string class instance
+    std::string text(pszText);
+
+    // Release the lock
+    GlobalUnlock(hData);
+
+    // Release the clipboard
+    CloseClipboard();
+
+    last = h;
+    h = text;
+    return text;
 }
 
-int getText()
+void setText(std::string in)
 {
-    if (OpenClipboard(NULL)) {
-
-        last = h;
-        h = GetClipboardData(CF_TEXT);
-        CloseClipboard();
-
-    }
-
-    return 0;
-}
-
-int setText(std::string *in)
-{
-    if (OpenClipboard(NULL)) {
-
-        h = in;
-        SetClipboardData(1, h);
-        CloseClipboard();
-
-    }
-
-    return 0;
-}
-*/
+    const size_t len = in.length() + 1;
+    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, len);
+    memcpy(GlobalLock(hMem), &in, len);
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
+}*/
 
 std::string translate(std::string tInput)
 {
-    int* toBeRemoved = new int[tInput.size()];
+    int *toBeRemoved = new int[tInput.size()];
     int position = 0;
     int uwuCounter = 0;
     for (int i = 0; i < tInput.size(); i++)
@@ -63,13 +71,15 @@ std::string translate(std::string tInput)
         }
         else if ((tInput[i] == 'T' || tInput[i] == 't')) //Replace the -> te, this -> tis
         {
-            if((i + 2) <= tInput.size() && (tInput[i + 1] == 'h' || tInput[i + 1] == 'H') && (tInput[i + 2] == 'e' || tInput[i + 2] == 'E')) {
+            if ((i + 2) <= tInput.size() && (tInput[i + 1] == 'h' || tInput[i + 1] == 'H') && (tInput[i + 2] == 'e' || tInput[i + 2] == 'E'))
+            {
                 tInput[i + 1] = 'e';
                 tInput[i + 2] = ' ';
                 toBeRemoved[position] = (i + 2);
                 position++;
             }
-            if((i + 3) <= tInput.size() && (tInput[i + 1] == 'h' || tInput[i + 1] == 'H') && (tInput[i + 2] == 'i' || tInput[i + 2] == 'I') && (tInput[i + 3] == 's' || tInput[i + 3] == 'S')) {
+            if ((i + 3) <= tInput.size() && (tInput[i + 1] == 'h' || tInput[i + 1] == 'H') && (tInput[i + 2] == 'i' || tInput[i + 2] == 'I') && (tInput[i + 3] == 's' || tInput[i + 3] == 'S'))
+            {
                 tInput[i] = 'D';
                 tInput[i + 1] = 'i';
                 tInput[i + 2] = 's';
@@ -80,7 +90,8 @@ std::string translate(std::string tInput)
         }
     }
 
-    for(int i = 0; i < position; i++) {
+    for (int i = 0; i < position; i++)
+    {
         tInput.erase(toBeRemoved[i], 1);
     }
 
@@ -94,14 +105,15 @@ std::string translate(std::string tInput)
     while (wunning)
     {
         getText();
-        if(h != last)
+        if (h != last)
         {
-            setText(translate(*(std::string *)h));
+            setText(translate(h));
         }
     }
-}*/ //Leads to segfault
+}*/
 
-std::string textBasedTranslation(std::string in) {
+std::string textBasedTranslation(std::string in)
+{
     std::string out = "";
     out = translate(in);
     return out;
